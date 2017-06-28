817800a0aa2bd0dc142b547d7adc25d3ce03459f
Jon Calhoun <joncalhoun@gmail.com>
2017-06-27 23:10:54 -0400

    Finished 2.1

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Lens Locked
 
 An awesome photo gallery application written in Go!
+
+
+ps - technically this file wasn't created in the book, but I am including it to give a little life to the repo when you visit it.
--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	http.HandleFunc("/", handlerFunc)
+	http.ListenAndServe(":3000", nil)
 }
