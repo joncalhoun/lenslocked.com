caf011f5ade016c64c23217c776e225738017de7
Jon Calhoun <joncalhoun@gmail.com>
2017-06-28 02:36:08 -0400

    Completed 3.1 in the book

--- main.go
+++ main.go
@@ -6,7 +6,20 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+	w.Header().Set("Content-Type", "text/html")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprint(w, "To get in touch, please send an email "+
+			"to <a href=\"mailto:support@lenslocked.com\">"+
+			"support@lenslocked.com</a>")
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "<h1>We could not find the page you "+
+			"were looking for :(</h1>"+
+			"<p>Please email us if you keep being sent to an "+
+			"invalid page.</p>")
+	}
 }
 
 func main() {
